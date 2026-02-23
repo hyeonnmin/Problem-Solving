5e8c31be938da3d1984a84ceb2237de249b56984
정현민 <wjdgusals111@naver.com>
2026-02-23 15:02:57 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>	
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	
+	priority_queue<int> q;
+	for (int i = 0; i < N; ++i)
+	{
+		int input;
+		cin >> input;
+		if (input == 0)
+		{
+			if (q.empty())
+				cout << "0\n";
+			else
+			{
+				cout << q.top() << "\n";
+				q.pop();
+			}
+		}
+		else
+			q.push(input);
+	}
+}
