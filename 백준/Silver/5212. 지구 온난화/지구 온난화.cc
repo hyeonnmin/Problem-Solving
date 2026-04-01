4c2d42c60450273218387d36b1abb1af8be4c6f8
정현민 <wjdgusals111@naver.com>
2026-04-01 21:28:27 +0900

    [Silver II] Title: 지구 온난화, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.cc
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.cc
@@ -3,95 +3,62 @@
 
 using namespace std;
 
-int R, C;
-
-bool checkSea(vector<vector<bool>> land, int i, int j)
-{
-	if (i < 0 || j < 0 || i >= R || j >= C)
-		return true;
-	
-	if (land[i][j] == false)
-		return true;
-
-	return false;
-}
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+	int R, C;
 	cin >> R >> C;
 
-	vector<vector<bool>> land(R, vector<bool>(C));
-	vector<vector<bool>> newLand(R, vector<bool>(C));
+	vector<vector<char>> m(R + 2, vector<char>(C + 2, '.'));
+	vector<vector<char>> after_m(R + 2, vector<char>(C + 2, '.'));
 
-	for (int i = 0; i < R; ++i)
+	for (int i = 1; i <= R; ++i)
 	{
-		for (int j = 0; j < C; ++j)
+		string input;
+		cin >> input;
+		for (int j = 1; j <= C; ++j)
 		{
-			char c;
-			cin >> c;
-
-			if (c == '.')
-			{
-				land[i][j] = false;
-				newLand[i][j] = false;
-			}
-			else
-			{
-				land[i][j] = true;
-				newLand[i][j] = true;
-			}
+			m[i][j] = input[j - 1];
+			after_m[i][j] = input[j - 1];
 		}
 	}
 
-	int startRow = R;
-	int endRow = -1;
-	int startCol = C;
-	int endCol = -1;
-	for (int i = 0; i < R; ++i)
+	int min_row = 11, min_col = 11;
+	int max_row = -1, max_col = -1;
+
+	for (int i = 1; i <= R; ++i)
 	{
-		for (int j = 0; j < C; ++j)
+		for (int j = 1; j <= C; ++j)
 		{
-			if (land[i][j] == true)
+			if (m[i][j] == 'X')
 			{
 				int count = 0;
-				if (checkSea(land, i - 1, j))
-					count++;
-				if (checkSea(land, i + 1, j))
-					count++;
-				if (checkSea(land, i, j - 1))
-					count++;
-				if (checkSea(land, i, j + 1))
-					count++;
+				
+				count += (m[i - 1][j] == '.' ? 1 : 0);
+				count += (m[i][j - 1] == '.' ? 1 : 0);
+				count += (m[i + 1][j] == '.' ? 1 : 0);
+				count += (m[i][j + 1] == '.' ? 1 : 0);
 
 				if (count >= 3)
-					newLand[i][j] = false;
+					after_m[i][j] = '.';
 				else
 				{
-					if (startRow > i)
-						startRow = i;
-					if (startCol > j)
-						startCol = j;
-					if (endRow < i)
-						endRow = i;
-					if (endCol < j)
-						endCol = j;
+					min_row = min_row > i ? i : min_row;
+					min_col = min_col > j ? j : min_col;
+					max_row = max_row > i ? max_row : i;
+					max_col = max_col > j ? max_col : j;
 				}
 			}
 		}
 	}
-	for (int i = startRow; i <= endRow; ++i)
+
+	for (int i = min_row; i <= max_row; ++i)
 	{
-		for (int j = startCol; j <= endCol; ++j)
-		{
-			if (newLand[i][j] == false)
-				cout << ".";
-			else
-				cout << "X";
-		}
+		for (int j = min_col; j <= max_col; ++j)
+			cout << after_m[i][j];
 		cout << '\n';
 	}
 }
