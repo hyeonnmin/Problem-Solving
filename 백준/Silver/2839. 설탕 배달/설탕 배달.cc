d6109bae81a4dc31be0dac48a79684ca83c70aba
정현민 <wjdgusals111@naver.com>
2026-04-09 21:16:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,27 +8,49 @@ int main()
 	int N;
 	cin >> N;
 
-	int three_bag = 0;
-	int five_bag = 0;
+	//int three_bag = 0;
+	//int five_bag = 0;
 
-	
-	five_bag = N / 5;
-	int rest = N - 5 * five_bag;
+	//
+	//five_bag = N / 5;
+	//int rest = N - 5 * five_bag;
 
-	while (rest % 3 != 0)
+	//while (rest % 3 != 0)
+	//{
+	//	if (rest == N)
+	//	{
+	//		cout << -1;
+	//		return 0;
+	//	}
+
+	//	five_bag--;
+	//	rest = N - 5 * five_bag;
+	//}
+
+	//three_bag = rest / 3;
+
+	//cout << three_bag + five_bag;
+
+	vector<int> record(N + 1, -1);
+	record[0] = 0;
+
+	for (int i = 3; i <= N; ++i)
 	{
-		if (rest == N)
+		if (i == 3)
+			record[3] = 1;
+		else if (i == 4)
+			record[4] = -1;
+		else
 		{
-			cout << -1;
-			return 0;
+			if (record[i - 5] != -1)
+				record[i] = record[i - 5] + 1;
+			else if (record[i - 3] != -1)
+				record[i] = record[i - 3] + 1;
+			else
+				record[i] = -1;
 		}
-
-		five_bag--;
-		rest = N - 5 * five_bag;
 	}
 
-	three_bag = rest / 3;
-
-	cout << three_bag + five_bag;
+	cout << record[N];
 
 }
