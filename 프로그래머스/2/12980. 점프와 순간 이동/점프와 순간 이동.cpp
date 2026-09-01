84d9ccfe7a00143675ae70591e941a3341deef18
정현민 <wjdgusals111@naver.com>
2026-09-01 17:50:02 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 4.73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+   
+    while(n != 1)
+    {
+        if(n % 2 == 1)
+            ans++;
+        
+        n /= 2;
+    }
+    
+    ans += 1;
+    
+    return ans;
+}
